4f4032494a8bac6894fb12217a55d0127372fde9
HallofFame1 <122780060+HallofFame1@users.noreply.github.com>
2023-01-30 00:16:44 +0300

    created a file called user_mathout.py and entered the output in it

--- user_mathout.py
+++ user_mathout.py
@@ -1,5 +1,5 @@
-# thon311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/user_math.py"
-# Area :  882
-# Price of dog food with tax:  175.5   
-# Order amount:  171.0
-# Sum of donations:  109.60000000000001
+thon311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/user_math.py"
+Area :  882
+Price of dog food with tax:  175.5   
+Order amount:  171.0
+Sum of donations:  109.60000000000001
