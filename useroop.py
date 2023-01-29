d37089a0f8f1ad5dee099a8a72075c1ca7fddf6b
HallofFame1 <122780060+HallofFame1@users.noreply.github.com>
2023-01-30 00:13:20 +0300

    completed task 1,2 and 3 also made change to the user_math.py

--- useroop.py
+++ useroop.py
@@ -1,110 +1,110 @@
-"""
-Helen Hammond,January 24, 2023, Module 2-Functions and Statistics
+# """
+# Helen Hammond,January 24, 2023, Module 2-Functions and Statistics
 
-"""
-import datetime
-from enum import Enum
-
-
-class Species(Enum):
-    MAN = 1
-    LADY = 2
-    ELF = 3
-    ORC = 4
+# """
+# import datetime
+# from enum import Enum
 
 
-class PyBuddy:
-    """ PyBuddy class for creating a study buddy."""
-
-    def __init__(self, name, species, num_legs, weight_kgs, country, is_available, skill_list):
-        """ Built-in method to create a new instance."""
-        self.name = name
-        self.species = species
-        self.num_legs = num_legs
-        self.weight_kgs = weight_kgs
-        self.country=country
-        self.is_available = is_available
-        self.skill_list = skill_list
-        self.create_date = datetime.datetime.now()
-
-    def __str__(self):
-        """Built-in method to return a string describing this instance"""
-        s0 = f"I'm {self.name}.\n"
-        s1 = f"I'm a {self.species} with {self.num_legs} legs.\n"
-        s2 = f"I weigh {self.weight_kgs:.2f} kgs.\n"
-        s3 = f"I've been alive for {self.get_age_string()}.\n"
-
-        if self.is_available:
-            s4 = "I'm available for tutoring.\n"
-        else:
-            s4 = "I'm already helping others learn Python.\n"
-
-        s5 = "I know:\n"
-
-        s6 = ""
-        for skill in self.skill_list:
-            s6 = s6 + f"  - {skill}\n"
-
-        s = s0 + s1 + s2 + s3 + s4 + s5 + s6
-        return s
-
-    def get_age_string(self):
-        """Return the age as a string."""
-        start = self.create_date
-        end = datetime.datetime.now()
-        duration = end - start
-        ageString = str(duration)
-        return ageString
-
-    def display_welcome(self):
-        print()
-        print("Welcome, I'm Helen and I'm good in python.\n")
-        # print using our built-in to string method
-        print(self.__str__())
-
-        final_message = """
-
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
-
-        Let's get started!
-
-        """
-        print(final_message)
+# class Species(Enum):
+#     MAN = 1
+#     LADY = 2
+#     ELF = 3
+#     ORC = 4
 
 
-# -------------------------------------------------------------
-# Call some functions and execute code!
+# class PyBuddy:
+#     """ PyBuddy class for creating a study buddy."""
 
-# if this is the main file being run
+#     def __init__(self, name, species, num_legs, weight_kgs, country, is_available, skill_list):
+#         """ Built-in method to create a new instance."""
+#         self.name = name
+#         self.species = species
+#         self.num_legs = num_legs
+#         self.weight_kgs = weight_kgs
+#         self.country=country
+#         self.is_available = is_available
+#         self.skill_list = skill_list
+#         self.create_date = datetime.datetime.now()
 
-if __name__ == "__main__":
-    # Create an instance of a PyBuddy
-    alice = PyBuddy(
-        "Helen",
-        Species.LADY,
-        2,
-        40.123456,
-        "United States",
-        True,
-        ["Git", "GitHub", "Python", "Markdown", "VS Code"],
-    )
+#     def __str__(self):
+#         """Built-in method to return a string describing this instance"""
+#         s0 = f"I'm {self.name}.\n"
+#         s1 = f"I'm a {self.species} with {self.num_legs} legs.\n"
+#         s2 = f"I weigh {self.weight_kgs:.2f} kgs.\n"
+#         s3 = f"I've been alive for {self.get_age_string()}.\n"
 
-    # Call the buddy's welcome() method
-    alice.display_welcome()
+#         if self.is_available:
+#             s4 = "I'm available for tutoring.\n"
+#         else:
+#             s4 = "I'm already helping others learn Python.\n"
+
+#         s5 = "I know:\n"
+
+#         s6 = ""
+#         for skill in self.skill_list:
+#             s6 = s6 + f"  - {skill}\n"
+
+#         s = s0 + s1 + s2 + s3 + s4 + s5 + s6
+#         return s
+
+#     def get_age_string(self):
+#         """Return the age as a string."""
+#         start = self.create_date
+#         end = datetime.datetime.now()
+#         duration = end - start
+#         ageString = str(duration)
+#         return ageString
+
+#     def display_welcome(self):
+#         print()
+#         print("Welcome, I'm Helen and I'm good in python.\n")
+#         # print using our built-in to string method
+#         print(self.__str__())
+
+#         final_message = """
+
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
+
+#         Let's get started!
+
+#         """
+#         print(final_message)
 
 
-    # Create another instance of a PyBuddy
-    # using named arguments so it's clear what we're doing
-    rex = PyBuddy(
-        name="Robert",
-        species=Species.MAN,
-        num_legs=2,
-        weight_kgs=40.437241,
-        country="United States",
-        is_available=True,
-        skill_list=["Git", "GitHub", "Python", "Markdown", "VS Code"],
-    )
+# # -------------------------------------------------------------
+# # Call some functions and execute code!
 
-    rex.display_welcome()
+# # if this is the main file being run
+
+# if __name__ == "__main__":
+#     # Create an instance of a PyBuddy
+#     alice = PyBuddy(
+#         "Helen",
+#         Species.LADY,
+#         2,
+#         40.123456,
+#         "United States",
+#         True,
+#         ["Git", "GitHub", "Python", "Markdown", "VS Code"],
+#     )
+
+#     # Call the buddy's welcome() method
+#     alice.display_welcome()
+
+
+#     # Create another instance of a PyBuddy
+#     # using named arguments so it's clear what we're doing
+#     rex = PyBuddy(
+#         name="Robert",
+#         species=Species.MAN,
+#         num_legs=2,
+#         weight_kgs=40.437241,
+#         country="United States",
+#         is_available=True,
+#         skill_list=["Git", "GitHub", "Python", "Markdown", "VS Code"],
+#     )
+
+#     rex.display_welcome()
--- useroopout.py
+++ useroopout.py
@@ -1,236 +1,236 @@
-Welcome, I'm a new PyBuddy.
+# Welcome, I'm a new PyBuddy.
 
-I'm Alice.
-I'm a Species.CAT with 4 legs.
-I weigh 8.12 kgs.
-I've been alive for 0:00:00.000999.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Alice.
+# I'm a Species.CAT with 4 legs.
+# I weigh 8.12 kgs.
+# I've been alive for 0:00:00.000999.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
-        Let's get started!
+#         Let's get started!
 
 
 
-Welcome, I'm a new PyBuddy.
+# Welcome, I'm a new PyBuddy.
 
-I'm Rex.
-I'm a Species.DOG with 4 legs.
-I weigh 10.44 kgs.
-I've been alive for 0:00:00.000998.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Rex.
+# I'm a Species.DOG with 4 legs.
+# I weigh 10.44 kgs.
+# I've been alive for 0:00:00.000998.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
-        Let's get started!
+#         Let's get started!
 
-PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
-Traceback (most recent call last):
-  File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 85, in <module>
-    Species.LADY,
-    ^^^^^^^^^^^^
-  File "C:\Users\USER\AppData\Local\Programs\Python\Python311\Lib\enum.py", line 787, in __getattr__
-    raise AttributeError(name) from None
-AttributeError: LADY
-PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
+# PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
+# Traceback (most recent call last):
+#   File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 85, in <module>
+#     Species.LADY,
+#     ^^^^^^^^^^^^
+#   File "C:\Users\USER\AppData\Local\Programs\Python\Python311\Lib\enum.py", line 787, in __getattr__
+#     raise AttributeError(name) from None
+# AttributeError: LADY
+# PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
 
-Welcome, I'm Helen and I'm good in python.
+# Welcome, I'm Helen and I'm good in python.
 
-I'm Helen.
-I'm a Species.LADY with 2 legs.
-I weigh 40.12 kgs.
-I've been alive for 0:00:00.000998.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Helen.
+# I'm a Species.LADY with 2 legs.
+# I weigh 40.12 kgs.
+# I've been alive for 0:00:00.000998.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
-        Let's get started!
+#         Let's get started!
 
 
 
-Welcome, I'm Helen and I'm good in python.
+# Welcome, I'm Helen and I'm good in python.
 
-I'm Rex.
-I'm a Species.DOG with 4 legs.
-I weigh 10.44 kgs.
-I've been alive for 0:00:00.000998.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Rex.
+# I'm a Species.DOG with 4 legs.
+# I weigh 10.44 kgs.
+# I've been alive for 0:00:00.000998.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
-        Let's get started!
+#         Let's get started!
 
 
-PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
-
-Welcome, I'm Helen and I'm good in python.
-
-I'm Helen.
-I'm a Species.LADY with 2 legs.
-I weigh 40.12 kgs.
-I've been alive for 0:00:00.001996.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
+
+# Welcome, I'm Helen and I'm good in python.
+
+# I'm Helen.
+# I'm a Species.LADY with 2 legs.
+# I weigh 40.12 kgs.
+# I've been alive for 0:00:00.001996.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
 
 
-Traceback (most recent call last):
-  File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 100, in <module>
-    species=Species.MAN,
-            ^^^^^^^^^^^
-  File "C:\Users\USER\AppData\Local\Programs\Python\Python311\Lib\enum.py", line 787, in __getattr__
-    raise AttributeError(name) from None
-AttributeError: MAN
-PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
+# Traceback (most recent call last):
+#   File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 100, in <module>
+#     species=Species.MAN,
+#             ^^^^^^^^^^^
+#   File "C:\Users\USER\AppData\Local\Programs\Python\Python311\Lib\enum.py", line 787, in __getattr__
+#     raise AttributeError(name) from None
+# AttributeError: MAN
+# PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
 
-Welcome, I'm Helen and I'm good in python.
+# Welcome, I'm Helen and I'm good in python.
 
-I'm Helen.
-I'm a Species.LADY with 2 legs.
-I weigh 40.12 kgs.
-I've been alive for 0:00:00.016955.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Helen.
+# I'm a Species.LADY with 2 legs.
+# I weigh 40.12 kgs.
+# I've been alive for 0:00:00.016955.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
-        Let's get started!
+#         Let's get started!
 
 
 
-Welcome, I'm Helen and I'm good in python.
+# Welcome, I'm Helen and I'm good in python.
 
-I'm Robert.
-I'm a Species.MAN with 2 legs.
-I weigh 40.44 kgs.
-I've been alive for 0:00:00.012987.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Robert.
+# I'm a Species.MAN with 2 legs.
+# I weigh 40.44 kgs.
+# I've been alive for 0:00:00.012987.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
-        Let's get started!
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
+#         Let's get started!
 
 
-PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
-  File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 104
-            ^^^^^^^^^^^^^^^
-SyntaxError: invalid syntax. Perhaps you forgot a comma?
-PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
-Traceback (most recent call last):
-  File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 84, in <module>
-    alice = PyBuddy(
-            ^^^^^^^^
-TypeError: PyBuddy.__init__() missing 1 required positional argument: 'skill_list'
-PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
+# PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
+#   File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 104
+#             ^^^^^^^^^^^^^^^
+# SyntaxError: invalid syntax. Perhaps you forgot a comma?
+# PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
+# Traceback (most recent call last):
+#   File "c:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions\useroop.py", line 84, in <module>
+#     alice = PyBuddy(
+#             ^^^^^^^^
+# TypeError: PyBuddy.__init__() missing 1 required positional argument: 'skill_list'
+# PS C:\Users\USER\OneDrive\Desktop\Github Project\Assignment-2\datafun-02-functions> & C:/Users/USER/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/USER/OneDrive/Desktop/Github Project/Assignment-2/datafun-02-functions/useroop.py"
 
-Welcome, I'm Helen and I'm good in python.
+# Welcome, I'm Helen and I'm good in python.
 
-I'm Helen.
-I'm a Species.LADY with 2 legs.
-I weigh 40.12 kgs.
-I've been alive for 0:00:00.000998.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Helen.
+# I'm a Species.LADY with 2 legs.
+# I weigh 40.12 kgs.
+# I've been alive for 0:00:00.000998.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
-        Let's get started!
+#         Let's get started!
 
 
 
-Welcome, I'm Helen and I'm good in python.
+# Welcome, I'm Helen and I'm good in python.
 
-I'm Robert.
-I'm a Species.MAN with 2 legs.
-I weigh 40.44 kgs.
-I've been alive for 0:00:00.002993.
-I'm available for tutoring.
-I know:
-  - Git
-  - GitHub
-  - Python
-  - Markdown
-  - VS Code
+# I'm Robert.
+# I'm a Species.MAN with 2 legs.
+# I weigh 40.44 kgs.
+# I've been alive for 0:00:00.002993.
+# I'm available for tutoring.
+# I know:
+#   - Git
+#   - GitHub
+#   - Python
+#   - Markdown
+#   - VS Code
 
 
 
-        You'll need curiousity, the ability to search the web,
-        and the tenacity and resourcefulness
-        to solve all kinds of challenges.
+#         You'll need curiousity, the ability to search the web,
+#         and the tenacity and resourcefulness
+#         to solve all kinds of challenges.
 
-        Let's get started!
+#         Let's get started!
